81c057e543f4b1d701eefa27ab33892894953661
Luka Monzon <lucamonzon2003@gmail.com>
2024-07-12 15:26:19 -0300

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -16,8 +16,8 @@ const CONFIG = {
       header: 'Github Projects',
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
-        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        sortBy: 'updated', // Sort projects by 'stars' or 'updated'
+        limit: 12, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
           projects: ['lucamonzon2003/lucamonzon2003', 'lucamonzon2003/Chat_And_StockForm'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
